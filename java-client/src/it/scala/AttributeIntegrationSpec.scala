030d4ed6512b589a87d368c9038788d2c3e99d81
schleichardt <michael.schleichardt@commercetools.de>
2014-06-03 14:18:26 +0200

    Use class tags in AttributeIntegrationSpec.

--- java-client/src/it/scala/AttributeIntegrationSpec.scala
+++ java-client/src/it/scala/AttributeIntegrationSpec.scala
@@ -13,6 +13,7 @@ import java.lang.{Double => JDouble}
 import java.math.{BigDecimal => JBigDecimal, BigInteger}
 import org.joda.time.DateTime
 import org.joda.time.format.{DateTimeFormatter, DateTimeFormat}
+import scala.reflect.ClassTag
 
 class AttributeIntegrationSpec extends WordSpec with MustMatchers {
   implicit lazy val client: SphereClient = IntegrationTestClient()
@@ -33,42 +34,42 @@ class AttributeIntegrationSpec extends WordSpec with MustMatchers {
 
       "read LocalizableEnum" in {
         val expected = ImmutableSet.of(lEnum("first", "first label en"), lEnum("second", "second label en"))
-        attributeMustBe(expected, "setattributelocenum", classOf[LocalizableEnum])
+        attributeMustBe(expected, "setattributelocenum")
       }
 
       "read LocalizedString" in {
         val expected = ImmutableSet.of("foo bar localized string", "second loc string", "third loc string").toEnLocSet
-        attributeMustBe(expected, "setattributelocstring", classOf[LocalizedString])
+        attributeMustBe(expected, "setattributelocstring")
       }
 
       "read text" in {
         val expected = ImmutableSet.of("text content 1", "text content 2")
-        attributeMustBe(expected, "SETATTRIBUTETEXT", classOf[String])
+        attributeMustBe(expected, "SETATTRIBUTETEXT")
       }
 
       "read enum" in {
         val expected = ImmutableSet.of(new Attribute.Enum("f1", "f1"), new Attribute.Enum("f2", "f2"))
-        attributeMustBe(expected, "setattributeenum", classOf[Attribute.Enum])
+        attributeMustBe(expected, "setattributeenum")
       }
 
       "read Integer" in {
         val expected = ImmutableSet.of(new Integer(21), new Integer(42))
-        attributeMustBe(expected, "SETATTRIBUTENUMBER", classOf[Integer])
+        attributeMustBe(expected, "SETATTRIBUTENUMBER")
       }
 
       "read Double" in {
         val expected = ImmutableSet.of(new JDouble(5), new JDouble(11.2))
-        attributeMustBe(expected, "SETATTRIBUTENUMBERDOUBLE", classOf[JDouble])
+        attributeMustBe(expected, "SETATTRIBUTENUMBERDOUBLE")
       }
 
       "read Money" in {
         val expected = ImmutableSet.of("500.55" EUR, "2.23" EUR)
-        attributeMustBe(expected, "SETATTRIBUTEMONEY", classOf[Money])
+        attributeMustBe(expected, "SETATTRIBUTEMONEY")
       }
 
       "read DateTime from date" in {
         val expected = ImmutableSet.of("2014-06-02T00:00:00.000+02:00" toDateTime, "2014-06-03T00:00:00.000+02:00" toDateTime)
-        attributeMustBe(expected, "setattributedate", classOf[DateTime])
+        attributeMustBe(expected, "setattributedate")
       }
 
       "read DateTime from time" in {
@@ -84,14 +85,14 @@ class AttributeIntegrationSpec extends WordSpec with MustMatchers {
 
       "read DateTime from date time" in {
         val expected = ImmutableSet.of("2014-06-02T15:47:16.000+02:00" toDateTime, "2014-06-03T15:47:24.000+02:00" toDateTime)
-        attributeMustBe(expected, "setattributedatetim", classOf[DateTime])
+        attributeMustBe(expected, "setattributedatetim")
       }
     }
   }
 
-  def attributeMustBe[T](expected: java.util.Set[T], attributeName: String, clazz: Class[T])(implicit product: Product) {
+  def attributeMustBe[T](expected: java.util.Set[T], attributeName: String)(implicit product: Product, tpe: ClassTag[T]) {
     val attribute: Attribute = product.getAttribute(attributeName.toLowerCase)
-    val actual: ImmutableSet[T] = attribute.getSet(clazz)
+    val actual = attribute.getSet(tpe.runtimeClass)
     actual must be(expected)
   }
 }
