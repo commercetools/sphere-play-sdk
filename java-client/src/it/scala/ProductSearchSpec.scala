960f9c2fb96de9762b5ba0017af96cd585829732
schleichardt <michael.schleichardt@commercetools.de>
2014-06-30 10:05:09 +0200

    Harden sphere.ProductSearchSpec for previous failed executions.

--- java-client/src/it/scala/ProductSearchSpec.scala
+++ java-client/src/it/scala/ProductSearchSpec.scala
@@ -5,6 +5,8 @@ import io.sphere.client.ProductSort
 import org.scalatest._
 import io.sphere.client.shop.model.{Attribute, ProductUpdate}
 
+import scala.util.Try
+
 class ProductSearchSpec extends WordSpec with MustMatchers {
   val client = IntegrationTestClient()
 
@@ -27,18 +29,28 @@ class ProductSearchSpec extends WordSpec with MustMatchers {
     }
     
     "set and remove attribute" in {
-      val product = client.products().byId("e44b3743-9e8d-4ce9-be57-b34ea9ec53e5").fetch().get()
-      val attribute = new Attribute("custom-attribute", "value1")
-      
+      def fetchProduct = client.products.byId("e44b3743-9e8d-4ce9-be57-b34ea9ec53e5").fetch.get
+
+      val attributeName = "custom-attribute"
+      val attribute = new Attribute(attributeName, "value1")
+
+      def deleteAttribute() = {
+        val product = fetchProduct
+        val cmd = new ProductUpdate().removeAttribute(product.getMasterVariant.getId, attributeName, false)
+        client.products().updateProduct(product.getIdAndVersion, cmd).execute
+      }
+
+      Try(deleteAttribute())
+      val product = fetchProduct
+
       info("set attribute")
       val setAttribute = new ProductUpdate().setAttribute(product.getMasterVariant.getId, attribute, false)
       val attributeSet = client.products().updateProduct(product.getIdAndVersion, setAttribute).execute()
-      attributeSet.getAttribute(attribute.getName) must be (attribute)
+      attributeSet.getAttribute(attributeName) must be (attribute)
 
       info("remove attribute")
-      val unsetAttribute = new ProductUpdate().removeAttribute(product.getMasterVariant.getId, attribute.getName, false)
-      val attributeUnset = client.products().updateProduct(attributeSet.getIdAndVersion, unsetAttribute).execute()
-      attributeUnset.getAttribute(attribute.getName) must be (null)
+      val attributeUnset = deleteAttribute()
+      attributeUnset.getAttribute(attributeName) must be (null)
     }
   }
 }
