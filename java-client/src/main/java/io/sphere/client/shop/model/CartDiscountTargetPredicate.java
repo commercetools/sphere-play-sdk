40d38a9497a6740cf39b1a24f1bcbdf371278b15
schleichardt <michael.schleichardt@commercetools.de>
2014-12-12 16:51:32 +0100

    Remove unused class.

--- java-client/src/main/java/io/sphere/client/shop/model/CartDiscountTargetPredicate.java
+++ java-client/src/main/java/io/sphere/client/shop/model/CartDiscountTargetPredicate.java
@@ -8,7 +8,6 @@ import org.codehaus.jackson.annotate.JsonTypeInfo;
         include = JsonTypeInfo.As.PROPERTY,
         property = "type")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = LineItemsTargetPredicate.class, name = "lineItems"),
-        @JsonSubTypes.Type(value = ShippingTargetPredicate.class, name = "shipping") })
+        @JsonSubTypes.Type(value = LineItemsTargetPredicate.class, name = "lineItems")})
 public abstract class CartDiscountTargetPredicate {
 }
