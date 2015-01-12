0aef1fc2a7ac1eb3205dfedb0eddd6026e9d3895
schleichardt <michael.schleichardt@commercetools.de>
2015-01-12 17:59:33 +0100

    Try to publish Javadoc with travis ci. #109

---
+++ scripts/publish-javadoc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    sbt "gitPublish target/javaunidoc https://$GH_TOKEN:x-oauth-basic@github.com/$TRAVIS_REPO_SLUG.git \"javadoc/$TRAVIS_BRANCH\" sphere-oss automation@commercetools.de";
+fi
